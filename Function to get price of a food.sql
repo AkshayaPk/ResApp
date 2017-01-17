480fde440d134ee173953fdeb6b923f064172b04
AkshayaPk <ashpeekay23@gmail.com>
2017-01-17 10:33:14 +0530

    Add files via upload

---
+++ Function to get price of a food.sql
@@ -0,0 +1,14 @@
+DELIMITER $$
+
+USE `restaurant`$$
+
+DROP FUNCTION IF EXISTS `FN_GET_PRICE_OF_FOOD`$$
+
+CREATE DEFINER=`root`@`localhost` FUNCTION `FN_GET_PRICE_OF_FOOD`(foodnamepara VARCHAR(50)) RETURNS INT(11)
+BEGIN
+    DECLARE price INT;
+         SELECT `food_name_type`.`priceOfFood` INTO price FROM `food_name_type` WHERE `food_name_type`.`foodName`=foodnamepara;
+         RETURN price;
+    END$$
+
+DELIMITER ;
